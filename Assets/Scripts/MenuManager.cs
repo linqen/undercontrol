438a6d24b07098ea45314aca8fe6dfdf6050c0c9
linqen <avena47@gmail.com>
2017-09-12 22:06:59 -0300

    ***Initial Commit***
    Simple menues and basic classes created

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : GenericSingletonClass<MenuManager> {
+
+	GameObject mapSelect;
+	GameObject characterSelect;
+	GameObject mainMenu;
+
+
+	// Use this for initialization
+	void Start () {
+		mapSelect = transform.Find ("MapSelect").gameObject;
+		mainMenu = transform.Find ("MainMenu").gameObject;
+		characterSelect = transform.Find ("CharacterSelect").gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayButton(){
+		mainMenu.SetActive (false);
+		mapSelect.SetActive (true);
+	}
+
+}
