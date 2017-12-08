401a1c8ca9afc8e9ab81f56830c566590d90c389
linqen <avena47@gmail.com>
2017-12-08 16:13:32 -0300

    *Adapt game to support more aspects ratios*

---
+++ Assets/Scripts/ScaleCameraWidth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleCameraWidth : MonoBehaviour {
+	public bool maintainWidth=true;
+	public float normalAspectWidth;
+	public float normalAspectHeight;
+	[Range(-1,1)]
+	public int adaptPosition;
+
+	float defaultWidth;
+	float defaultHeight;
+	
+
+	Vector3 CameraPos;
+
+	void Start () {
+		CameraPos = Camera.main.transform.position;
+
+		defaultHeight = Camera.main.orthographicSize;
+		defaultWidth = Camera.main.orthographicSize * (normalAspectWidth/normalAspectHeight);
+	}
+
+	void Update () {
+
+		if (maintainWidth) {
+			Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+
+			Camera.main.transform.position= new Vector3(CameraPos.x,CameraPos.y + adaptPosition*(defaultHeight-Camera.main.orthographicSize),CameraPos.z);
+
+		} else {
+			Camera.main.transform.position= new Vector3(CameraPos.x + adaptPosition*(defaultWidth-Camera.main.orthographicSize*Camera.main.aspect) ,CameraPos.y,CameraPos.z);
+
+		}
+
+
+	}
+}
